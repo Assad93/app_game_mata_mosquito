0b08731b513112b8e2dc5b0a28d677b3da4ccb87
SamirAssad-coder <samir_caverna@hotmail.com>
2020-05-15 07:36:08 -0300

    inserindo imagens e definindo a dimensão do palco do jogo

---
+++ jogo.js
@@ -0,0 +1,11 @@
+var altura = 0;
+var largura = 0;
+
+function ajustaTamanhoPalcoJogo(params) {
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+    console.log(altura, largura)
+}
+
+
+
